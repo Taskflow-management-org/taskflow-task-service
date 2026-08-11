72efd98dcaaea0bd891c3a04d2c735c189b402b7
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-11 20:13:23 +0530

    feat: add Task entity with JPA mappings, enums, timestamps, and user assignment fields

--- src/main/java/com/thamindu/task_manager/entity/Task.java
+++ src/main/java/com/thamindu/task_manager/entity/Task.java
@@ -1,4 +1,55 @@
 package com.thamindu.task_manager.entity;
 
+import com.thamindu.task_manager.enums.TaskPriority;
+import com.thamindu.task_manager.enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TaskPriority priority;
+
+    @Column(name = "assigned_user_id")
+    private UUID assignedUserId;
+
+    @Column(name = "created_by_user_id", nullable = false)
+    private UUID createdUserId;
+
+    @Column(name = "due_date")
+    private LocalDateTime dueDate;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
