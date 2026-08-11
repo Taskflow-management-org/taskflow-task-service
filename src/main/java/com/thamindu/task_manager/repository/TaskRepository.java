f1626035f822c9f09844bf925b2a1fec2f96d512
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-11 20:15:41 +0530

    feat: add TaskRepository with queries for assigned user and task status

--- src/main/java/com/thamindu/task_manager/repository/TaskRepository.java
+++ src/main/java/com/thamindu/task_manager/repository/TaskRepository.java
@@ -1,4 +1,15 @@
 package com.thamindu.task_manager.repository;
 
-public interface TaskRepository {
+import com.thamindu.task_manager.entity.Task;
+import com.thamindu.task_manager.enums.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
+
+
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByAssignedUserId(UUID assignedUserId);
+
+    List<Task> findByStatus(TaskStatus status);
+
 }
